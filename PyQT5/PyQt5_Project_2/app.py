67dc01ddca96c014c92cb2b48e1a7b9db3196452
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-11-28 17:47:16 +0100

    Do something with project 2. Not working. Create project 3: My_C2WS

--- PyQT5/PyQt5_Project_2/app.py
+++ PyQT5/PyQt5_Project_2/app.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
 
+# Following this: https://www.youtube.com/watch?v=MOItX2aKTGc
 def main():
     app = QApplication([])
     window = QWidget()
@@ -11,13 +12,21 @@ def main():
 
     label = QLabel("Press the button below")
     button = QPushButton("Press me !")
+    
+    label.setText("Press the button below")
+    label.setFont(QFont("Arial", 16))
+    label.move(50,100)
+
+    button.clicked.connect(on_clicked)
 
     layout.addWidget(label)
     layout.addWidget(button)
-    
 
     window.show()
     app.exec_()
+
+def on_clicked():
+    print("Hello world")
 
 if __name__ == '__main__':
     main()
