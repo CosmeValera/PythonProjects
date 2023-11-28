18d859c7b38500e3b498dd37bce6542607d838c1
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-11-28 14:19:05 +0100

    First project working properly

--- PyQT5/PyQt5_Project_1/app.py
+++ PyQT5/PyQt5_Project_1/app.py
@@ -6,6 +6,20 @@ class PyQt5_Project_1(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("gui_app.ui", self)
+        self.disableBut.setEnabled(False)
+        self.enableBut.clicked.connect(self.enable)
+        self.disableBut.clicked.connect(self.disable)
+
+    def enable(self):
+        self.enableBut.setEnabled(False)
+        self.disableBut.setEnabled(True)
+        self.enableLab.setText("Enabled")
+
+    def disable(self):
+        self.enableBut.setEnabled(True)
+        self.disableBut.setEnabled(False)
+        self.enableLab.setText("Disabled")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
