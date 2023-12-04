0fcfd5a62e18f798569ad1d4626ca6667dbadfdf
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-12-04 16:52:02 +0100

    filter working for table

--- PyQT5/PyQt5_Project_6_Search_Filter/main.py
+++ PyQT5/PyQt5_Project_6_Search_Filter/main.py
@@ -1,13 +1,56 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QListWidget, QLabel, QPushButton
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QTableWidget, QTableWidgetItem, QCheckBox
+from PyQt5.QtCore import Qt
+from functools import partial
 from qtawesome import icon
+
+class MyTableWidget(QTableWidget):
+    def __init__(self, parent, headers, data):
+        super().__init__(parent)
+        self.setSelectionBehavior(QTableWidget.SelectRows)
+        self.cellClicked.connect(partial(self.select_session, parent))
+
+        self.headers = headers
+        self.setColumnCount(len(self.headers))
+        self.setColumnWidth(0, 70)
+        self.setColumnWidth(1, 100)
+        self.setColumnWidth(2, 270)
+        self.setHorizontalHeaderLabels(self.headers)
+
+        self.data = data
+        self.set_data()
+
+    def update_from_list(self, data_list):
+        self.clearContents()
+        self.data = data_list
+        self.set_data()
+
+    def set_data(self):
+        self.setRowCount(len(self.data))
+        for index, session in enumerate(self.data):
+            # Display data in table (modify as needed)
+            self.setItem(index, 0, QTableWidgetItem(str(session.get("fav", ""))))
+            self.setItem(index, 1, QTableWidgetItem(str(session.get("elem", ""))))
+            self.setItem(index, 2, QTableWidgetItem(str(session.get("ws", ""))))
+            self.setItem(index, 3, QTableWidgetItem(str(session.get("prot", ""))))
+            self.setItem(index, 4, QTableWidgetItem(str(session.get("user", ""))))
+
+    def select_session(self, view):
+        selected_row_index = self.selectedIndexes()[0].row()
+        view.selected_session = self.data[selected_row_index]
+        # Add any additional logic for handling the selected session
 
 class DataFilterApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.data = ["Apple", "Banana", "Orange", "Grapes", "Cherry", "Strawberry", "Blueberry", "Raspberry"]
+        self.headers = ["Fav", "Element", "Workstation", "Protocol", "User"]
+        self.data = [
+            {"fav": False, "elem": "Manzana", "ws": "WS1", "prot": "Protocol1", "user": "User1"},
+            {"fav": True, "elem": "Portatil", "ws": "WS2", "prot": "UDP", "user": "User2"},
+            {"fav": True, "elem": "Raqueta", "ws": "WS3", "prot": "TCPIP", "user": "Joan"},
+            {"fav": False, "elem": "Granada", "ws": "WS4", "prot": "IPV24", "user": "Helena"},
+        ]
         self.filtered_data = self.data.copy()
 
         self.init_ui()
@@ -17,43 +60,28 @@ class DataFilterApp(QWidget):
 
         self.filter_input = QLineEdit(self)
         self.filter_input.setPlaceholderText("Type to filter...")
-
-        # Set font for the filter input
-        font = QFont()
-        font.setBold(True)
-        self.filter_input.setFont(font)
-
-        # Add clear button to the filter input
         self.filter_input.setClearButtonEnabled(True)
+
 
         # Add a search icon from FontAwesome to the filter input
         search_icon = icon('fa.search', color='black')
         self.filter_input.addAction(search_icon, QLineEdit.LeadingPosition)
-
-        # Connect the filter signal to the update_filter method
         self.filter_input.textChanged.connect(self.update_filter)
 
-        self.data_list = QListWidget(self)
-        self.update_data_list()
+        self.table_widget = MyTableWidget(self, self.headers, self.filtered_data)
 
-        layout.addWidget(QLabel("Filter:"))
         layout.addWidget(self.filter_input)
-        layout.addWidget(QLabel("Filtered Data:"))
-        layout.addWidget(self.data_list)
+        layout.addWidget(self.table_widget)
 
         self.setLayout(layout)
 
-        self.setGeometry(100, 100, 400, 300)
+        self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle('Data Filter App')
         self.show()
 
-    def update_data_list(self):
-        self.data_list.clear()
-        self.data_list.addItems(self.filtered_data)
-
     def update_filter(self, text):
-        self.filtered_data = [item for item in self.data if text.lower() in item.lower()]
-        self.update_data_list()
+        self.filtered_data = [item for item in self.data if text.lower() in str(item).lower()]
+        self.table_widget.update_from_list(self.filtered_data)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
