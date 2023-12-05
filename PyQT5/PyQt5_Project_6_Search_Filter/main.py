79f4d13c1a77792a7fa1a26cb6f9e279ce4cc975
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-12-05 11:22:09 +0100

    button showing last session

--- PyQT5/PyQt5_Project_6_Search_Filter/main.py
+++ PyQT5/PyQt5_Project_6_Search_Filter/main.py
@@ -1,17 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QTableWidget, QTableWidgetItem, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QTableWidget, QTableWidgetItem, QCheckBox, QPushButton
 from PyQt5.QtCore import Qt
 from functools import partial
 from qtawesome import icon
 from qt_material import apply_stylesheet
 
 class MyTableWidget(QTableWidget):
-    def __init__(self, parent, headers, data):
+    def __init__(self, parent, data):
         super().__init__(parent)
         self.setSelectionBehavior(QTableWidget.SelectRows)
         self.cellClicked.connect(partial(self.select_session, parent))
 
-        self.headers = headers
+        self.headers = ["Fav", "Element", "Workstation", "Protocol", "User"]
         self.setColumnCount(len(self.headers))
         self.setColumnWidth(0, 70)
         self.setColumnWidth(1, 100)
@@ -45,14 +45,12 @@ class MyTableWidget(QTableWidget):
     def select_session(self, view):
         selected_row_index = self.selectedIndexes()[0].row()
         view.selected_session = self.data[selected_row_index]
-        # Add any additional logic for handling the selected session
 
 class DataFilterApp(QWidget):
     def __init__(self):
         super().__init__()
         apply_stylesheet(self, theme="gmvTheme.xml")
 
-        self.headers = ["Fav", "Element", "Workstation", "Protocol", "User"]
         self.data = [
             {"fav": False, "elem": "Manzana", "ws": "WS1", "prot": "Protocol1", "user": "User1"},
             {"fav": True, "elem": "Portatil", "ws": "WS2", "prot": "UDP", "user": "User2"},
@@ -69,16 +67,18 @@ class DataFilterApp(QWidget):
         self.filter_input = QLineEdit(self)
         self.filter_input.setPlaceholderText("Type to filter...")
         self.filter_input.setClearButtonEnabled(True)
-
-        # Add a search icon from FontAwesome to the filter input
         search_icon = icon('fa.search', color='white')
         self.filter_input.addAction(search_icon, QLineEdit.LeadingPosition)
         self.filter_input.textChanged.connect(self.update_filter)
 
-        self.table_widget = MyTableWidget(self, self.headers, self.filtered_data)
+        self.table_widget = MyTableWidget(self, self.filtered_data)
+        
+        self.print_button = QPushButton("Print Selected Session", self)
+        self.print_button.clicked.connect(self.print_selected_session)
 
         layout.addWidget(self.filter_input)
         layout.addWidget(self.table_widget)
+        layout.addWidget(self.print_button)
 
         self.setLayout(layout)
 
@@ -89,8 +89,14 @@ class DataFilterApp(QWidget):
     def update_filter(self, text):
         self.filtered_data = [item for item in self.data if text.lower() in str(item).lower()]
         self.table_widget.update_from_list(self.filtered_data)
+    
+    def print_selected_session(self):
+        if hasattr(self, 'selected_session'):
+            print(self.selected_session)
+        else:
+            print("No session selected.")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = DataFilterApp()
+    DataFilterApp()
     sys.exit(app.exec_())
