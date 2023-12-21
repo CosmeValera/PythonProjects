fd0e84867de0683441c199c68241c80b10ac17b3
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-12-21 14:31:29 +0100

    Allow default value, and 3 headers work to fold rows

--- PyQT5/PyQt5_Project_11_group_toggles_names/main2.py
+++ PyQT5/PyQt5_Project_11_group_toggles_names/main2.py
@@ -5,20 +5,22 @@ class GroupedTableWidget(QWidget):
     def __init__(self):
         super(GroupedTableWidget, self).__init__()
 
+        self.original_data = [
+            {"id": "1", "number": "one", "name": "pepe"},
+            {"id": "2", "number": "two", "name": "javi"},
+            {"id": "3", "number": "one", "name": "javi"},
+            {"id": "4", "number": "five", "name": "javi"},
+            {"id": "1", "number": "four", "name": "marcos"}
+        ]
+
         self.table_widget = QTableWidget(self)
         self.table_widget.setColumnCount(3)
         self.table_widget.setHorizontalHeaderLabels(["id", "number", "name"])
 
-        self.data = [
-            {"id": "1", "number": "one", "name": "pepe"},
-            {"id": "2", "number": "two", "name": "javi"},
-            {"id": "3", "number": "three", "name": "javi"}
-        ]
-
         self.populate_table()
 
         self.column_selector = QComboBox(self)
-        self.column_selector.addItems(["id", "number", "name"])
+        self.column_selector.addItems(["", "id", "number", "name"])
         self.column_selector.currentIndexChanged.connect(self.group_by_column)
 
         layout = QVBoxLayout(self)
@@ -26,48 +28,51 @@ class GroupedTableWidget(QWidget):
         layout.addWidget(self.table_widget)
 
     def populate_table(self):
-        self.table_widget.setRowCount(len(self.data))
+        self.table_widget.setRowCount(len(self.original_data))
 
-        for row, item in enumerate(self.data):
+        for row, item in enumerate(self.original_data):
             for col, value in enumerate(item.values()):
                 self.table_widget.setItem(row, col, QTableWidgetItem(value))
 
     def group_by_column(self, index):
         column_name = self.column_selector.itemText(index)
 
-        if column_name == "name":
-            self.group_by_name()
+        self.restore_original_data()
+        if column_name in {"id", "name", "number"}:
+            self.group_by_column_value(column_name)
 
-    def group_by_name(self):
-        name_column_index = 2  # Assuming "name" is the third column
+    def group_by_column_value(self, column_name):
+        column_index = {"id": 0, "number": 1, "name": 2}[column_name]
 
         grouped_data = {}
         for row in range(self.table_widget.rowCount()):
-            name_item = self.table_widget.item(row, name_column_index)
+            column_item = self.table_widget.item(row, column_index)
 
-            name_value = name_item.text()
-            if name_value not in grouped_data:
-                grouped_data[name_value] = {"id": set(), "number": set()}
+            column_value = column_item.text()
+            if column_value not in grouped_data:
+                grouped_data[column_value] = {"id": set(), "number": set(), "name": set()}
 
-            id_item = self.table_widget.item(row, 0)  # Assuming "id" is the first column
-            number_item = self.table_widget.item(row, 1)  # Assuming "number" is the second column
-
-            grouped_data[name_value]["id"].add(id_item.text())
-            grouped_data[name_value]["number"].add(number_item.text())
+            for col in range(self.table_widget.columnCount()):
+                value = self.table_widget.item(row, col).text()
+                grouped_data[column_value][self.table_widget.horizontalHeaderItem(col).text()].add(value)
 
         # Clear the table
         self.table_widget.setRowCount(0)
 
         # Add grouped data to the table
-        for name_value, columns in grouped_data.items():
+        for column_value, columns in grouped_data.items():
             self.table_widget.insertRow(self.table_widget.rowCount())
 
-            id_text = ", ".join(sorted(columns["id"]))
-            number_text = ", ".join(sorted(columns["number"]))
+            for col, values in enumerate(columns.values()):
+                text = ", ".join(sorted(values))
+                self.table_widget.setItem(self.table_widget.rowCount() - 1, col, QTableWidgetItem(text))
 
-            self.table_widget.setItem(self.table_widget.rowCount() - 1, 0, QTableWidgetItem(id_text))
-            self.table_widget.setItem(self.table_widget.rowCount() - 1, 1, QTableWidgetItem(number_text))
-            self.table_widget.setItem(self.table_widget.rowCount() - 1, 2, QTableWidgetItem(name_value))
+    def restore_original_data(self):
+        self.table_widget.setRowCount(0)
+        for row, item in enumerate(self.original_data):
+            self.table_widget.insertRow(row)
+            for col, value in enumerate(item.values()):
+                self.table_widget.setItem(row, col, QTableWidgetItem(value))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
