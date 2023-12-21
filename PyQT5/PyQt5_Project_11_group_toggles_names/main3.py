da4ab533600ad81ea4d564e4058864e39370c9c0
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-12-21 15:44:52 +0100

    main3 doing sometihng pretty good, table up, combobox, table bottom

---
+++ PyQT5/PyQt5_Project_11_group_toggles_names/main3.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QComboBox, QTreeView
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
+class MyWindow(QMainWindow):
+    def __init__(self, data):
+        super().__init__()
+        self.setWindowTitle("PyQt5 Table, ComboBox, and TreeView Example")
+        self.setGeometry(100, 100, 800, 600)
+
+        # Create QTableWidget
+        self.tableWidget = QTableWidget(self)
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setHorizontalHeaderLabels(["id", "number", "name"])
+        self.addItemsToTable(data)
+
+        # Create QComboBox
+        self.comboBox = QComboBox(self)
+        self.comboBox.addItems(["", "id", "number", "name"])
+        self.comboBox.currentIndexChanged.connect(self.updateTreeView)
+
+        # Create QTreeView
+        self.treeView = QTreeView(self)
+        self.model = QStandardItemModel()
+        self.treeView.setModel(self.model)
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.tableWidget)
+        layout.addWidget(self.comboBox)
+        layout.addWidget(self.treeView)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def addItemsToTable(self, data):
+        for row, item in enumerate(data):
+            self.tableWidget.insertRow(row)
+            for col, value in enumerate(item.values()):
+                self.tableWidget.setItem(row, col, QTableWidgetItem(str(value)))
+
+    def updateTreeView(self, index):
+        header = self.comboBox.currentText()
+        self.model.clear()
+        root_item = self.model.invisibleRootItem()
+
+        grouped_data = {}
+        for row in range(self.tableWidget.rowCount()):
+            item = self.tableWidget.item(row, self.comboBox.currentIndex())
+            value = item.text()
+            if value not in grouped_data:
+                grouped_data[value] = []
+            grouped_data[value].append([self.tableWidget.item(row, col).text() for col in range(3)])
+
+        for key, values in grouped_data.items():
+            category_item = QStandardItem(key)
+            for v in values:
+                category_item.appendRow([QStandardItem(val) for val in v])
+            root_item.appendRow(category_item)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    data = [
+        {"id": "1", "number": "one", "name": "pepe"},
+        {"id": "2", "number": "two", "name": "javi"},
+        {"id": "3", "number": "one", "name": "javi"},
+        {"id": "4", "number": "five", "name": "javi"},
+        {"id": "1", "number": "four", "name": "marcos"}
+    ]
+    window = MyWindow(data)
+    window.show()
+    sys.exit(app.exec_())
