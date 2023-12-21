b7864deb2dccbcd05436218da06d10f275aa3bb8
Cosme Valera Reales <cosmevalerareales@gmail.com>
2023-12-21 14:40:34 +0100

    add togle button not doing anything in each row

--- PyQT5/PyQt5_Project_11_group_toggles_names/main2.py
+++ PyQT5/PyQt5_Project_11_group_toggles_names/main2.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QComboBox
+from PyQt5.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QComboBox, QPushButton
 
 class GroupedTableWidget(QWidget):
     def __init__(self):
@@ -10,12 +10,12 @@ class GroupedTableWidget(QWidget):
             {"id": "2", "number": "two", "name": "javi"},
             {"id": "3", "number": "one", "name": "javi"},
             {"id": "4", "number": "five", "name": "javi"},
-            {"id": "1", "number": "four", "name": "marcos"}
+            {"id": "1", "number": "two", "name": "marcos"}
         ]
 
         self.table_widget = QTableWidget(self)
-        self.table_widget.setColumnCount(3)
-        self.table_widget.setHorizontalHeaderLabels(["id", "number", "name"])
+        self.table_widget.setColumnCount(4)  # Additional column for the button
+        self.table_widget.setHorizontalHeaderLabels(["id", "number", "name", ""])
 
         self.populate_table()
 
@@ -33,6 +33,10 @@ class GroupedTableWidget(QWidget):
         for row, item in enumerate(self.original_data):
             for col, value in enumerate(item.values()):
                 self.table_widget.setItem(row, col, QTableWidgetItem(value))
+
+            fold_button = QPushButton("Toggle")
+            fold_button.clicked.connect(lambda _, r=row: self.toggle_rows(r))
+            self.table_widget.setCellWidget(row, 3, fold_button)
 
     def group_by_column(self, index):
         column_name = self.column_selector.itemText(index)
@@ -52,7 +56,7 @@ class GroupedTableWidget(QWidget):
             if column_value not in grouped_data:
                 grouped_data[column_value] = {"id": set(), "number": set(), "name": set()}
 
-            for col in range(self.table_widget.columnCount()):
+            for col in range(self.table_widget.columnCount() - 1):  # Exclude the button column
                 value = self.table_widget.item(row, col).text()
                 grouped_data[column_value][self.table_widget.horizontalHeaderItem(col).text()].add(value)
 
@@ -67,12 +71,31 @@ class GroupedTableWidget(QWidget):
                 text = ", ".join(sorted(values))
                 self.table_widget.setItem(self.table_widget.rowCount() - 1, col, QTableWidgetItem(text))
 
+            fold_button = QPushButton("Toggle")
+            fold_button.clicked.connect(lambda _, v=column_value: self.toggle_group(v))
+            self.table_widget.setCellWidget(self.table_widget.rowCount() - 1, 3, fold_button)
+
     def restore_original_data(self):
         self.table_widget.setRowCount(0)
         for row, item in enumerate(self.original_data):
             self.table_widget.insertRow(row)
             for col, value in enumerate(item.values()):
                 self.table_widget.setItem(row, col, QTableWidgetItem(value))
+
+            fold_button = QPushButton("Toggle")
+            fold_button.clicked.connect(lambda _, r=row: self.toggle_rows(r))
+            self.table_widget.setCellWidget(row, 3, fold_button)
+
+    def toggle_group(self, column_value):
+        for row in range(self.table_widget.rowCount()):
+            if self.table_widget.item(row, 0).text() == column_value:
+                self.table_widget.setRowHidden(row, not self.table_widget.isRowHidden(row))
+
+    def toggle_rows(self, row):
+        if self.table_widget.isRowHidden(row):
+            self.table_widget.setRowHidden(row, False)
+        else:
+            self.table_widget.setRowHidden(row, True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
