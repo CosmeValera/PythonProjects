b7e65e6b63b0494fa706fb89ffbd2df6b4cd9096
Cosme Valera Reales <cosmevalerareales@gmail.com>
2024-01-09 18:09:46 +0100

    tags being added as empty

--- PyQT5/PyQt5_Project_13_tags/main93_droppablearea.py
+++ PyQT5/PyQt5_Project_13_tags/main93_droppablearea.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QMainWindow, QFrame
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QMainWindow, QFrame
 from PyQt5.QtCore import Qt, QMimeData
 from PyQt5.QtGui import QDrag
 
@@ -16,16 +16,42 @@ class TagBar(QWidget):
         super().__init__()
         self.setAcceptDrops(True)
         self.tags = []
+        self.h_layout = QHBoxLayout(self)
 
     def dragEnterEvent(self, event):
         event.accept()
 
     def dropEvent(self, event):
         print('Something was dropped in the TagBar')
-        tag = QFrame(self)
-        self.tags.append(tag)
-        print(f'Created new tag. Now there are {len(self.tags)} tags.')
+        self.add_tag_to_bar(text)
         event.accept()
+
+    def add_tag_to_bar(self, text):
+        tag = QFrame()
+        tag.setStyleSheet('border:1px solid rgb(192, 192, 192); border-radius: 4px;')
+        tag.setContentsMargins(2, 2, 2, 2)
+        tag.setFixedHeight(28)
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(4, 4, 4, 4)
+        tag_label = QLabel(text)
+        hbox.addWidget(tag_label)
+        close_button = QPushButton('x')
+        close_button.setFixedSize(20, 20)
+        close_button.clicked.connect(lambda: self.remove_tag(text))
+        hbox.addWidget(close_button)
+        tag.setLayout(hbox)
+        self.h_layout.addWidget(tag)
+        self.tags.append(tag)
+
+    def remove_tag(self, text):
+        for tag in self.tags:
+            if tag.children()[1].text() == text:
+                self.h_layout.removeWidget(tag)
+                tag.deleteLater()
+                self.tags.remove(tag)
+                break
+        print(f'Removed tag. Now there are {len(self.tags)} tags.')
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
